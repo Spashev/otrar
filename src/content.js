dfa23d0408f24fee2c918a16222d0b87d6fd1c29
Spashev Nurken <s.nurken92@gmail.com>
2019-10-18 21:00:42 +0600

    fix: match

--- src/content.js
+++ src/content.js
@@ -6,20 +6,21 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
     if( request.todo == "changeColor") {
         var matches = [
             {
-                'id': 1, 
-                'color': '#da0e0e', 
-                'pattern': /(\d+)\s+(\w{2})(\s?\d{1,4})\s+(\w{1,4})\s+(\w{3})\s+(\d+\s)?(\w{3})\s+(\d+\s+)?(\d{4})\s+(\d{4})\s+(\d+)\s+(\d{2}[A-Z]{3}\d{2})\s+(\d{2}[A-Z]{3}\d{2})\s+(\w+)\s+(\d{1,2}\:\d{1,2})/g
+                id: 1, 
+                color: '#da0e0e', 
+                pattern: /(\d{1,2})\s+(\w{2})(\s?\d{1,4})\s+(\w{1,4})\s+(\w{3})\s+(\d+\s)?(\w{3})\s+(\d+\s+)?(\d{4})\s+(\d{4})\s+(\d+)\s+(\d{2}[A-Z]{3}\d{2})\s+(\d{2}[A-Z]{3}\d{2})\s+(\w+)\s+(\d{1,2}\:\d{1,2})/
             }
         ];// change matches
         var tags = document.getElementsByClassName('mg1');
         tags.forEach(function(items){
-            matches.forEach(function(element) {
+            for(let element of matches) {
                 var tag = items.innerHTML;
                 var result = tag.match(element.pattern);
+                console.log(element);
                 if(result) { 
-                    items.style.color = request.clickedColor;
+                    items.style.backgroundColor = request.clickedColor;
                 }
-            });
+            }
         });
     }
 });
