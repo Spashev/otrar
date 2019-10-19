5f575401e66ccb4861d401e0e095c1745624de3d
Damir Alimkhan <alimkhan.damir@gmail.com>
2019-10-19 12:51:25 +0600

    added axios import

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App'
-
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+    
+Vue.use(VueAxios, axios)
 
 /* eslint-disable no-new */
 new Vue({
